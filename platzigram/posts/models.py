ab0bc30b70252be9d3328c22f3abeb5cd618e783
bcespedes <bcespedes@monssa.com.ar>
2019-08-09 16:53:30 -0300

    app users

--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+# class User(models.Model):
+# 	"""User model"""
+# 	# Django agrega id por defecto
+
+# 	email    = models.EmailField(unique=True)
+# 	password = models.CharField(max_length=100)
+	
+# 	first_name = models.CharField(max_length=100)
+# 	last_name  = models.CharField(max_length=100)
+
+# 	is_admin = models.BooleanField(default=False)
+	
+# 	bio = models.TextField(blank=True)
+	
+# 	birthDate = models.DateField(blank=True, null=True)
+# 	created   = models.DateField(auto_now_add=True)
+# 	modified  = models.DateTimeField(auto_now=True)
+
+# 	def __str__(self):
+# 		return self.email
